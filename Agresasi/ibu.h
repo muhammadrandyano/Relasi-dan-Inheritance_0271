509827a3eac2a2f098d880d7460ee31bbbb4f4ac
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-05-27 10:34:06 +0700

    first commit

---
+++ Agresasi/ibu.h
@@ -0,0 +1,32 @@
+#ifndef IBU_H
+#define IBU_H
+
+class ibu {
+    public :
+        string nama;
+        vector<anak*> daftar_anak;
+
+        ibu (string pNama): nama(pNama){
+            cout << "ibu\"" << nama << "\" ada\n;
+        }
+         
+        ~ibu(){
+            cout << "ibu\"" << nama << "\" tidak ada\n";
+        }
+
+        void tambahAnak(anak*);
+        void cetakAnak();
+};
+
+void ibu ::tambahAnak(anak* pAnak){
+    daftar_anak.push_back(pAnak);
+}
+
+void ibu ::cetakAnak(){
+    cout << "Daftar Anak dari IBU \"" << this->nama<< "\":\n";
+    for (int i=0; i < daftar_anak.size(); i++){
+        cout << daftar_anak->nama<<endl;
+    }
+    cout << endl; 
+}
+#endif
