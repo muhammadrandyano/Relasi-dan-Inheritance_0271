9a1868612f47bcdc83a19bb68126ce0c7c5bf8ff
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-05-27 11:48:01 +0700

    Membuat class pasien

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -3,3 +3,17 @@
 using namespace std;
 
 class dokter;
+
+class pasien{
+    public :
+        string nama;
+        vector<dokter*> daftar_dokter;
+        pasien(string pNama): nama(pNama){
+                cout << "Pasien \"" << nama << "\" ada \n";
+        }
+        ~pasien(){
+           cout << "Pasien \"" << nama << "\" Tidak ada \n"; 
+        }
+        void tambahDokter(dokter*);
+        void cetakDokter();
+};
