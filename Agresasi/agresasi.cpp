0e479edeb455a3a2496dc50453c505a55601e2a0
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-05-27 11:42:34 +0700

    Membuat var ibu dan anak

--- Agresasi/agresasi.cpp
+++ Agresasi/agresasi.cpp
@@ -4,3 +4,10 @@ using namespace std;
 
 #include "anak.h"
 #include "ibu.h"  
+
+int main(){
+    ibu*varibu = new ibu("dini");
+    ibu*varibu2 = new ibu("novi");
+    anak*varAnak1 = new anak ("tono");
+    anak*varAnak2 = new anak ("rini");
+    anak*varAnak3 = new anak ("dewi");
