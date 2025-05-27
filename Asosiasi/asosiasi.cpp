09965df935bbd49a75601c004f8b72162a132ee5
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-05-27 11:49:50 +0700

    Membuat void pasien dan dokter

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -32,3 +32,27 @@ class dokter {
         void tambahPasien(pasien*);
         void cetakPasien();
 };
+
+void pasien::tambahDokter(dokter* pDokter) {
+    daftar_dokter.push_back(pDokter);
+}
+
+void pasien ::cetakDokter(){
+    cout << "Daftar dokter yang menangani pasien\""<<this->nama << "\":\n";
+    for(auto& a : daftar_dokter){
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
+
+void dokter::tambahPasien(pasien* pPasien){
+    daftar_pasien.push_back(pPasien);
+}
+
+void dokter::cetakPasien(){
+     cout << "Daftar pasien dari dokter\""<<this->nama << "\":\n";
+    for(auto& a : daftar_pasien){
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
